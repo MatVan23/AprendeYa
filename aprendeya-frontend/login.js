33bb3eec0f48b3dfa3842c3a5865f711b4493f44
mateo villegas <mateorito@gmail.com>
2026-06-11 00:22:58 -0400

    fix url registro

--- aprendeya-frontend/login.js
+++ aprendeya-frontend/login.js
@@ -10,7 +10,7 @@ async function login() {
 
   try {
 
-    const res = await fetch(`${API}/usuarios`, {
+    const res = await fetch(`${API}/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
